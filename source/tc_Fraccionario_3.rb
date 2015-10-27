38eca53cd73213428da56d5248c33c57dfe42553
erikbv99421 <erikbv99421@gmail.com>
2015-10-27 13:17:50 +0000

    Creado el tercer fichero de pruebas unitarias tc_Fraccionario_3.rb

---
+++ source/tc_Fraccionario_3.rb
@@ -0,0 +1,29 @@
+require './fraccion'
+require 'test/unit'
+
+class Testfraccion < Test::Unit::TestCase
+
+  def setup
+		@f1 = Fraccionario.new(3,1)
+		@f2 = Fraccionario.new(4,2)
+		@f3 = @f1+@f2 
+		@f4 = @f1*@f2
+  end
+
+  def teardown
+    ## Nothing really
+  end
+
+  def test_simple
+    assert_equal(3, @f1.resultado)
+    assert_equal(2, @f2.resultado)
+  end
+
+  def test_add_failure
+    assert_equal(5, @f3.resultado, "Adding work" )
+  end
+
+  def test_multiply_work
+    assert_equal(6, @f4.resultado, "Multiply work" )
+  end
+end
